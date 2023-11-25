fde60fb58fbeaa7d6a2cc89b25dc9dbfc0232b15
jihun <jihun315016@gmail.com>
2023-11-26 01:15:47 +0900

    Add Solution DotNetCore6

---
+++ DotNetCore6/DotNetCore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
